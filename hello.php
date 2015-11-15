db8699451c6c4586af36747c8aac71b4c4e8beed
adhityah <adhit.nugrah@yahoo.com>
2015-11-15 13:02:10 +0700

    edit hello.php

--- hello.php
+++ hello.php
@@ -3,13 +3,13 @@
 		if($emty($_POST))
 		{
 			$nama = $_POST['name'];
-			$email = $_POST[email];
+			$email = $_POST['email'];
 		}
 	?>
 	
 	<body>
 		<h1><?php $nama ?></h1>
 	
-		<h3><?php $email ?></h3>
+		<h3><?php $email ?>x</h3>
 	</body>
 </html>
